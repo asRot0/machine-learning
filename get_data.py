23ba8da6ab23560944a2c2f9f785e1c35530d91b
asif <asahmed8890@gmail.com>
2024-08-12 20:54:46 +0600

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -3,13 +3,11 @@ import os
 
 dataset = './datasets/housing/housing.csv'
 
-
 # pd.set_option('display.max_columns', None)
 # pd.options.display.max_columns = None
 os.environ['COLUMNS'] = '150'
 
 data = pd.read_csv(dataset)
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 # gdp = pd.read_csv(data, thousands=',', delimiter='\t', na_values='n/a')
-
-print(df.head())
+print(data.head())
