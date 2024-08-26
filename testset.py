d617a2ad83230a2d9f7f1e2188cac1932d5b27d9
asif <asahmed8890@gmail.com>
2024-08-26 19:17:38 +0600

    Add files via upload

--- testset.py
+++ testset.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# os.environ['COLUMNS'] = '200'
+os.environ['COLUMNS'] = '200'
 dataset = 'datasets/housing/housing.csv'
 
 housing = pd.read_csv(dataset)
@@ -25,9 +25,19 @@ print(housing.head())
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 print(split)
 
+strat_train_set = None
+strat_test_set = None
+
 for train_index, test_index in split.split(housing, housing['income_cat']):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
 
 print(housing['income_cat'].value_counts() / len(housing))
 
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop('income_cat', axis=1, inplace=True)
+# print(strat_test_set.head())
+
+housing = strat_train_set.copy()
+housing.plot(kind='scatter', x='longitude', y='latitude', alpha=0.1)
+plt.show()
