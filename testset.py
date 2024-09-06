19db5f6ed363d658acd382bc28115c6b7428cc38
asif <asahmed8890@gmail.com>
2024-09-06 21:46:59 +0600

    Add files via upload

--- testset.py
+++ testset.py
@@ -181,10 +181,25 @@ print('error [decision tree regressor]:', tree_rmse)
 
 # using cross-validation for decision tree
 '''
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+'''
+'''
 scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
                          scoring='neg_mean_squared_error', cv=10)
 tree_rmse_scores = np.sqrt(-scores)
+display_scores(tree_rmse_scores)
 print(tree_rmse_scores.mean())
+'''
+
+# using cross-validation for  linear regression
+'''
+lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels,
+                             scoring="neg_mean_squared_error", cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
 '''
 
 # random forest regressor
