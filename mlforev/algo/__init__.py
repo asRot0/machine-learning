b79a9e9cfcd031269c7aae85a1f9a61ab8113d9a
asif <asahmed8890@gmail.com>
2024-09-20 21:36:41 +0600

    Add files via upload

---
+++ mlforev/algo/__init__.py
@@ -0,0 +1,10 @@
+"""
+ML Package Initialization
+
+Goal:
+This __init__.py file allows the directory to be treated as a package. It imports relevant classes for use.
+"""
+
+from .data_processing import DataProcessor
+from .knn_model import KNNModel
+from .naive_bayes_model import NaiveBayesModel
