040ef2c254c34b6d572d5d3a71ad7e855852d4a8
asif <asahmed8890@gmail.com>
2024-10-19 19:28:39 +0600

    Add files via upload

--- supervised/svm.py
+++ supervised/svm.py
@@ -4,7 +4,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
-
 iris = datasets.load_iris()
 print(iris.keys())
 print(iris.target_names, 'target_names')
@@ -12,8 +11,17 @@ print(iris.feature_names, 'feature_names')
 
 print(iris.data[:5], 'iris.data[:5]')
 
-X = iris['data'][:, (2, 3)]
+X = iris['data'][:, (2, 3)]  # petal length, petal width
 print(X[:5], 'X[:5]')
 
-y = (iris.target == 2).astype(int)
+y = (iris.target == 2).astype(int)  # Iris-Virginica
 print(y[:5], 'y[:5]')
+
+svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('linear_svc', LinearSVC())
+])
+
+svm_clf.fit(X, y)
+prediction = svm_clf.predict([[5.8, 1.7]])
+print('prediction_', ['Not Iris-Virginica', 'Iris-Virginica'][prediction[0]])
