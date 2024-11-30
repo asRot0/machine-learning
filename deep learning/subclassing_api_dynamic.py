c31f2201b9bbafa853060ad68353acd10cb49a4a
asif <asahmed8890@gmail.com>
2024-11-30 17:46:53 +0600

    Add files via upload

--- deep learning/subclassing_api_dynamic.py
+++ deep learning/subclassing_api_dynamic.py
@@ -1,3 +1,9 @@
+'''
+Allows for dynamic and completely custom models by subclassing the Model class.
+Use Case: Highly dynamic architectures or models requiring custom forward passes.
+'''
+
+
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Flatten
 
@@ -22,3 +28,15 @@ model = MyCustomModel()
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.build(input_shape=(None, 28, 28))  # Specify input shape for summary
 model.summary()
+
+
+'''
+Description:
+This script introduces the Subclassing API, which is the most flexible way to create dynamic and highly customized models by subclassing the tf.keras.Model class.
+
+Behind the Theory:
+    - The Subclassing API allows full control over the forward pass by overriding the call method.
+    - Ideal for implementing models that require dynamic behaviors, such as recursive networks or attention mechanisms.
+    - However, it requires careful handling of layer connections and is less suited for rapid prototyping compared to Sequential and Functional APIs.
+
+'''
