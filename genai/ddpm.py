6473f19ae0d868a979fccb53bee2f7c98dca12dc
asif <asahmed8890@gmail.com>
2025-07-23 21:56:43 +0600

    Initial Denoising Diffusion Probabilistic Models implementation

--- genai/ddpm.py
+++ genai/ddpm.py
@@ -27,3 +27,33 @@ num_res_blocks = 2  # number of residual blocks
 dataset_name = None
 splits = ["train"]
 train_ds = None
+
+
+class GaussianDiffusion:
+    """Gaussian diffusion utility.
+
+    Args:
+        beta_start: Start value of the scheduled variance
+        beta_end: End value of the scheduled variance
+        timesteps: Number of time steps in the forward process
+    """
+
+    def __init__(self, beta_start=1e-4, beta_end=0.02, timesteps=1000, clip_min=-1.0, clip_max=1.0):
+
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.timesteps = timesteps
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+
+        # linear variance schedule
+        self.betas = betas = np.linspace( beta_start, beta_end, timesteps, dtype=np.float64)
+        self.num_timesteps = int(timesteps)
+
+        alphas = 1.0 - betas
+        alphas_cumprod = np.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = np.append(1.0, alphas_cumprod[:-1])
+
+        self.betas = tf.constant(betas, dtype=tf.float32)
+        self.alphas_cumprod = tf.constant(alphas_cumprod, dtype=tf.float32)
+        self.alphas_cumprod_prev = tf.constant(alphas_cumprod_prev, dtype=tf.float32)
